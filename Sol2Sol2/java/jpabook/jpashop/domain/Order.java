4e91ecd9993fe8c364e4ba57f5608f629d4b602c
parksol <thfdl0317@naver.com>
2022-01-31 13:40:19 +0900

    스프링 부트와 JPA활용

--- Sol2Sol2/java/jpabook/jpashop/domain/Order.java
+++ Sol2Sol2/java/jpabook/jpashop/domain/Order.java
@@ -3,9 +3,11 @@ package jpabook.jpashop.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -18,24 +20,41 @@ import javax.persistence.Table;
 @Table(name = "orders")
 public class Order {
 
+
+
 	@Id @GeneratedValue
 	@Column(name = "order_id")
 	private Long id;
 	
 	
 	//Member와 Order는 다대일 관계이기에
-	@ManyToOne
+	@ManyToOne//(fetch = LAZY)
 	@JoinColumn(name="member_id")
 	private Member member;
 	
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 	
-	@OneToOne
+	@OneToOne//(fetch = LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "delivery_id")
 	@JoinColumn
 	private Delivery delivery;
 	
 	private LocalDataTime orderDate;
 	
 	private OrderStatus status; //주문상태 [order, cancel]
+	
+	//연관관계 메서드//
+	public void setMember(Member member) {
+		this.member=member;
+		member.getOrders().add(this);
+	}
+	
+	public static void main(String[] args) {
+		Member member = new Member();
+		Order order = new Order();
+		
+		member.getOrders().add(order);
+		order.setMember(member);
+	}
 }
