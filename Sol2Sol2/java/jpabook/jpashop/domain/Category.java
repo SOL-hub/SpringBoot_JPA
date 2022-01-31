77d0860c2c2bf914c9219a4ed5f0c55077d8265a
parksol <thfdl0317@naver.com>
2022-01-31 13:25:15 +0900

    JPA 스프링부트

--- Sol2Sol2/java/jpabook/jpashop/domain/Category.java
+++ Sol2Sol2/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -28,7 +29,7 @@ public class Category {
 		inverseJoinColumns = @JoinColumn(name="item_id"))  
 	private List<Item> items = new ArrayList<>();
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="paret_id")
 	private Category parent;
 	
