675a6e361df7dd5b2fe833ead674110844f4d924
parksol <thfdl0317@naver.com>
2022-01-31 20:15:13 +0900

    JPA 스프링부터 주문 리포지토리 개발

--- Sol2Sol2/java/jpabook/jpashop/domain/item/Item.java
+++ Sol2Sol2/java/jpabook/jpashop/domain/item/Item.java
@@ -1,61 +1,35 @@
 package jpabook.jpashop.domain.item;
-
+import jpabook.jpashop.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+import jpabook.jpashop.domain.Category;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-
-import jpabook.jpashop.domain.Category;
-
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public class Item {
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+	public abstract class Item {
+
 	@Id @GeneratedValue
-	 @Column(name = "item_id")
-	 private Long id;
-	 private String name;
-	 
-	 private int price;
-	 private int stockQuantity;
-	 
-	 
-	 private List<Category> categroyies = new ArrayList<>();
-	 
-	 
-	//setter getter
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getStockQuantity() {
-		return stockQuantity;
-	}
-	public void setStockQuantity(int stockQuantity) {
-		this.stockQuantity = stockQuantity;
-	}
-	 
-	
-	
-	 
-	 
-	 
+	@Column(name = "item_id")
+	private Long id;
+	private String name;
+	private int price;
+	private int stockQuantity;
+ @ManyToMany(mappedBy = "items")
+ private List<Category> categories = new ArrayList<Category>();
+ //==비즈니스 로직==//
+ public void addStock(int quantity) {
+ this.stockQuantity += quantity;
+ }
+ public void removeStock(int quantity) {
+ int restStock = this.stockQuantity - quantity;
+ if (restStock < 0) {
+ throw new NotEnoughStockException("need more stock");
+ }
+ this.stockQuantity = restStock;
+ }
 }
---
+++ Sol2Sol2/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ItemService {
+
+}
