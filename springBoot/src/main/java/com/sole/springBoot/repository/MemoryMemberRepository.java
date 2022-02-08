51be72bab7b6e7d1731501427c17eb08152243f8
parksol <thfdl0317@naver.com>
2022-02-08 17:50:51 +0900

    스프링 부트, 웹 MVC, DB 접근 기술 - 리포지토리 테스트 케이스 작성

--- springBoot/src/main/java/com/sole/springBoot/repository/MemoryMemberRepository.java
+++ springBoot/src/main/java/com/sole/springBoot/repository/MemoryMemberRepository.java
@@ -37,4 +37,9 @@ public class MemoryMemberRepository implements MemberRepository{
 		return new ArrayList<>(store.values());
 	}
 
+	public void clearStore() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
