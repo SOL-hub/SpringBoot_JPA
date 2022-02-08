5f76c254e7af00fc7bf64b3fa67ecbd57018a79e
parksol <thfdl0317@naver.com>
2022-02-08 18:50:43 +0900

    스프링 부트, 웹 MVC, DB 접근 기술 - 회원 서비스 개발

---
+++ springBoot/src/main/java/com/sole/springBoot/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.sole.springBoot.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sole.springBoot.domain.Member;
+import com.sole.springBoot.repository.MemberRepository;
+import com.sole.springBoot.repository.MemoryMemberRepository;
+
+public class MemberService {
+	
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	
+	//회원가입
+	public Long join(Member member) {
+		
+		//같은 이름이 있는 중복 회원은 안돼
+		validateDuplicateMember(member); //중복회원 검증하고
+		
+		memberRepository.save(member); //검증 후 저장
+		return member.getId();
+		
+		//회원가입하면 아이디를 반환해주겠다.
+		//근데 같은 이름이 있는 회원은 안된다.
+		
+	}
+
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+		.ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+	
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+		
+	}
+	
+	public Optional<Member> findOne(Long memberId){
+		return memberRepository.findById(memberId);
+	}
+}
