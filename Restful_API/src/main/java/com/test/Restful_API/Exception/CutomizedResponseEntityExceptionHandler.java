30ed54cd5e3685175fff79f487372351ccf8c068
parksol <thfdl0317@naver.com>
2022-03-09 04:43:23 +0900

    Spring의 AOP를 이용한 Exception Handling

---
+++ Restful_API/src/main/java/com/test/Restful_API/Exception/CutomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.test.Restful_API.Exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.test.Restful_API.domain.UserNotFoundException;
+
+@RestController
+@ControllerAdvice //에러가 발생한다면 이 핸들러에 등록된 에러메소드가 실행
+public class CutomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{ 
+	
+	//단일 조회 시 에러 상세정보파악위해
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	//UserNotFoundException가 발생한다면 에러코드 404로 변경위해
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+}
---
+++ Restful_API/src/main/java/com/test/Restful_API/Exception/ExceptionResponse.java
@@ -0,0 +1,47 @@
+package com.test.Restful_API.Exception;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+	//핸들러 예외클래스가 발생할 수 있도록 처리
+	//예외가 발생한 시간에 대한 정보
+	private Date timestamp;
+	private String message;
+	private String details;
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	
+	
+	
+}
